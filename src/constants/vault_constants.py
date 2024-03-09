c344aaf3dc2c8d37879d5796234e445f78feaf8f
JamesBond <introbond.dev@gmail.com>
2024-03-09 20:27:31 +0700

    Add new vault address for gmx-arb-glp in Beefy Vault

--- src/constants/vault_constants.py
+++ src/constants/vault_constants.py
@@ -9,9 +9,9 @@ BLACK_VAULT_ADDRESSES = {
 }
 
 BEEFY_VAULT_ADDRESSES = {
+    "beefy:vault:arbitrum:0x9dbbbaecacedf53d5caa295b8293c1def2055adc": "gmx-arb-glp",
     "beefy:vault:arbitrum:0x9790e09d26f73ee888dfc387b4d9aab79e88f280": "joe-auto-wbtc-weth",
     "beefy:vault:arbitrum:0x0fb132fa7118eba49a1b6c3dd50e9e88896a09bb": "equilibria-arb-glp",
     "beefy:vault:ethereum:0xbeef8e0982874e0292e6c5751c5a4092b3e1beef": "bifi-vault",
-    "beefy:vault:arbitrum:0x9dbbbaecacedf53d5caa295b8293c1def2055adc": "gmx-arb-glp",
     "beefy:vault:arbitrum:0x5b904f19fb9ccf493b623e5c8ce91603665788b0": "gmx-arb-gmx",
 }
