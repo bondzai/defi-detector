af7cacf0a4e956a26b0992ff87bd34c59d2febc6
JamesBond <introbond.dev@gmail.com>
2024-03-09 17:16:18 +0700

    Refactor BeefyDataFetcher process_data method

--- src/protocols/bifi.py
+++ src/protocols/bifi.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 from src.protocols import DefiProtocol
-from pprint import pprint
 
 
 class BeefyDataFetcher(DefiProtocol):
@@ -11,14 +10,11 @@ class BeefyDataFetcher(DefiProtocol):
         super().__init__(api_url, **params)
 
     def process_data(self):
-        response = self.fetch_data()
-        if response:
-            data = response
-
+        data = self.fetch_data()
+        if data:
             df = pd.DataFrame(data)
-
-            df_filtered = df[df['is_eol'] == False] 
-
-            pprint(df_filtered.to_dict(orient="records"))
+            df_filtered = df[df['is_eol'] == False]
+            print("Summary:")
+            print(len(df_filtered))
         else:
             print("No data fetched.")
