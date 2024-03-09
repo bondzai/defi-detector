ba7596424781f8c084d141ca44915b4b022150aa
JamesBond <introbond.dev@gmail.com>
2024-03-09 20:23:34 +0700

    Update vault addresses in vault_constants.py

--- src/constants/vault_constants.py
+++ src/constants/vault_constants.py
@@ -1,11 +1,11 @@
 BLACK_VAULT_ADDRESSES = {
-    "inj1rcjtkmfyymcdm03alslr890khuzmzn7jxy5zgt": "ETH",
-    "inj1cr939nkxcwtm6ru47nqpxu39dv6mlvvw6lhsrp": "DOJO",
-    "inj1jzk6f35uv86tu8t7a8qj2revdh6l6sxhy49k30": "SOL",
-    "inj1jtvgvktz7vx7yvk04ahwg54h0rd6llfknns20m": "BTC",
-    "inj1mrfsm3c8psn6vaznxh7ewgsp4zn5vldaq54lqu": "BNB",
-    "inj1vh4j56pf5x4sp8a3xklj0v3sq73y6cs4qvul7s": "INJ",
-    "inj1v4hl4s7lqw84usxqhh9mhjewlwvfe5r8zygvc3": "INJ-USDT",
+    "inj1jtvgvktz7vx7yvk04ahwg54h0rd6llfknns20m": "btc-usdt-trend-following",
+    "inj1rcjtkmfyymcdm03alslr890khuzmzn7jxy5zgt": "ETH-trend-following",
+    "inj1mrfsm3c8psn6vaznxh7ewgsp4zn5vldaq54lqu": "bnb-usdt-trend-following",
+    "inj1vh4j56pf5x4sp8a3xklj0v3sq73y6cs4qvul7s": "inj-usdt-trend-following",
+    "inj1jzk6f35uv86tu8t7a8qj2revdh6l6sxhy49k30": "sol-usdt-market-making",
+    "inj1v4hl4s7lqw84usxqhh9mhjewlwvfe5r8zygvc3": "inj-usdt-market-making",
+    "inj1cr939nkxcwtm6ru47nqpxu39dv6mlvvw6lhsrp": "dojo-inj-market-making",
 }
 
 BEEFY_VAULT_ADDRESSES = {
