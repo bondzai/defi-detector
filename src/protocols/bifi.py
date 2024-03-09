ac56c14d2600e921a98dbaf9ffb192f890167d6a
JamesBond <introbond.dev@gmail.com>
2024-03-09 20:01:58 +0700

    Refactor BeefyDataFetcher class to filter and print summary of valid product keys

--- src/protocols/bifi.py
+++ src/protocols/bifi.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 from src.protocols import DefiProtocol
-
+from src.constants.vault_constants import BIFI_VAULT_ADDRESSES
+from pprint import pprint
 
 class BeefyDataFetcher(DefiProtocol):
     def __init__(self, address):
@@ -15,14 +16,20 @@ class BeefyDataFetcher(DefiProtocol):
             df = pd.DataFrame(data)
             df = df[(df['is_eol'] == False) & (df['is_dashboard_eol'] == False)]
             df['datetime'] = pd.to_datetime(df['datetime'])
-            df_sorted = df.sort_values(by='datetime', ascending=False)
-            df_filtered = df_sorted.drop_duplicates(subset='product_key', keep='first')
+            df = df.sort_values(by='datetime', ascending=False)
+            df = df.drop_duplicates(subset='product_key', keep='first')
 
-            print(df_filtered)
+            valid_product_keys = [key for key in BIFI_VAULT_ADDRESSES.keys()]
+            df = df[df['product_key'].isin(valid_product_keys)]
+
+            df = df.drop(columns=[
+                "is_eol",
+                "is_dashboard_eol",
+                "product_key",
+                "transaction_hash",
+            ])
+
             print("Summary:")
-            print(len(df_filtered))
-
-            from pprint import pprint
-            pprint(df_filtered.to_dict(orient="records"))
+            pprint(df.to_dict(orient="records"))
         else:
             print("No data fetched.")
