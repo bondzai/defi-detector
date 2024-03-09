476b69bc894236449a352b1092ae10d9feaaf86f
JamesBond <introbond.dev@gmail.com>
2024-03-09 17:37:24 +0700

    Add Black Vault constants and update BlackDataFetcher to process data

---
+++ src/constants/vault_constants.py
@@ -0,0 +1,9 @@
+BLACK_VAULT_ADDRESSES = {
+    "inj1rcjtkmfyymcdm03alslr890khuzmzn7jxy5zgt": "ETH",
+    "inj1cr939nkxcwtm6ru47nqpxu39dv6mlvvw6lhsrp": "DOJO",
+    "inj1jzk6f35uv86tu8t7a8qj2revdh6l6sxhy49k30": "SOL",
+    "inj1jtvgvktz7vx7yvk04ahwg54h0rd6llfknns20m": "BTC",
+    "inj1mrfsm3c8psn6vaznxh7ewgsp4zn5vldaq54lqu": "BNB",
+    "inj1vh4j56pf5x4sp8a3xklj0v3sq73y6cs4qvul7s": "INJ",
+    "inj1v4hl4s7lqw84usxqhh9mhjewlwvfe5r8zygvc3": "INJ-USDT",
+}
--- src/protocols/black_panther.py
+++ src/protocols/black_panther.py
@@ -1,18 +1,35 @@
 import os
 import pandas as pd
 from src.protocols import DefiProtocol
-
+from src.constants.vault_constants import BLACK_VAULT_ADDRESSES
 
 class BlackDataFetcher(DefiProtocol):
     def __init__(self, wallet_address):
         api_url = os.getenv("BLACK_API_URL")
         params = {"wallet_address": wallet_address}
         super().__init__(api_url, **params)
+        self.vault_address = BLACK_VAULT_ADDRESSES
 
     def process_data(self):
         data = self.fetch_data()
         if data:
             df = pd.DataFrame(data)
+
+            df['vault_address_mapped'] = df['vault_address'].map(self.vault_address)
+
+            grouped_data = df.groupby('vault_address_mapped').agg(
+                total_previous_share_value=('previous_share_value', 'sum'),
+                total_current_share_value=('current_share_value', 'sum'),
+                total_performance=('performance', lambda x: x.sum() / len(x) if len(x) > 0 else 0)
+            )
+
+            for index, row in grouped_data.iterrows():
+                print(f"Vault Strategy: {index}")
+                print(f"Previous Share Value: {row['total_previous_share_value']:.2f}")
+                print(f"Current Share Value: {row['total_current_share_value']:.2f}")
+                print(f"Performance: {row['total_performance']:.2f}%")
+                print()
+
             total_previous_share_value = df['previous_share_value'].sum()
             total_current_share_value = df['current_share_value'].sum()
             total_performance = df['performance'].sum() / len(df) if len(df) > 0 else 0
@@ -23,3 +40,4 @@ class BlackDataFetcher(DefiProtocol):
             print(f"Performance: {total_performance:.2f}%")
         else:
             print("No data fetched.")
+
