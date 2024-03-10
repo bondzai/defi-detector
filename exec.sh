16ff3e483b734998e7b3deabd70cea03edef0b6c
JamesBond <introbond.dev@gmail.com>
2024-03-10 21:14:19 +0700

    Add cron job to execute script periodically

--- exec.sh
+++ exec.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+# Edit this file to introduce tasks to be run by cron.
+# 
+# Each task to run has to be defined through a single line
+# indicating with different fields when the task will be run
+# and what command to run for the task
+# 
+# To define the time you can provide concrete values for
+# minute (m), hour (h), day of month (dom), month (mon),
+# and day of week (dow) or use '*' in these fields (for 'any').
+# 
+# Notice that tasks will be started based on the cron's system
+# daemon's notion of time and timezones.
+# 
+# Output of the crontab jobs (including errors) is sent through
+# email to the user the crontab file belongs to (unless redirected).
+# 
+# For example, you can run a backup of all your user accounts
+# at 5 a.m every week with:
+# 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
+# 
+# For more information see the manual pages of crontab(5) and cron(8)
+# 
+# m h  dom mon dow   command
+#MAILTO=''
+#* * * * * /home/jb/Desktop/code/lab/defi-detector/exec.sh >> /home/jb/cron.log 2>&1
+
 # Change directory to the script's directory
 cd "$(dirname "$0")"
 
 source env/bin/activate
+
 python src/main.py
