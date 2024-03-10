b2576fc68e53ea622659ac52a565e58c27acee1d
JamesBond <introbond.dev@gmail.com>
2024-03-10 20:22:28 +0700

    Refactor Black protocol message formatting

--- src/constants/vault_constants.py
+++ src/constants/vault_constants.py
@@ -1,6 +1,6 @@
 BLACK_VAULT_ADDRESSES = {
     "inj1jtvgvktz7vx7yvk04ahwg54h0rd6llfknns20m": "btc-usdt-trend-following",
-    "inj1rcjtkmfyymcdm03alslr890khuzmzn7jxy5zgt": "ETH-trend-following",
+    "inj1rcjtkmfyymcdm03alslr890khuzmzn7jxy5zgt": "eth-trend-following",
     "inj1mrfsm3c8psn6vaznxh7ewgsp4zn5vldaq54lqu": "bnb-usdt-trend-following",
     "inj1vh4j56pf5x4sp8a3xklj0v3sq73y6cs4qvul7s": "inj-usdt-trend-following",
     "inj1jzk6f35uv86tu8t7a8qj2revdh6l6sxhy49k30": "sol-usdt-market-making",
--- src/protocols/black.py
+++ src/protocols/black.py
@@ -11,36 +11,39 @@ class Black(DefiProtocol):
         super().__init__(url=api_url, method="rest", **params)
 
     def process_data(self):
-        print("Processing Black data...", "\n")
+        print("Processing Black data...\n")
         data = self.fetch_data()
         if data:
             df = pd.DataFrame(data)
-
             df['vault_address_mapped'] = df['vault_address'].map(BLACK_VAULT_ADDRESSES)
-
             grouped_data = df.groupby('vault_address_mapped').agg(
                 total_previous_share_value=('previous_share_value', 'sum'),
                 total_current_share_value=('current_share_value', 'sum'),
                 total_performance=('performance', lambda x: x.sum() / len(x) if len(x) > 0 else 0)
             )
 
+            messages = []
+            messages.append("Black Panther Protocol\n\n")
             for index, row in grouped_data.iterrows():
-                message = (f"Vault Strategy: {index}\n"
-                           f"Previous Share Value: {row['total_previous_share_value']:.2f}\n"
-                           f"Current Share Value: {row['total_current_share_value']:.2f}\n"
+                message = (f"{index}\n"
+                           f"Previous: {row['total_previous_share_value']:.2f}\n"
+                           f"Current: {row['total_current_share_value']:.2f}\n"
                            f"Performance: {row['total_performance']:.2f}%\n\n")
-                self.send_message(message, platforms=['line'])
+                messages.append(message)
 
             previous_share_value = df['previous_share_value'].sum()
             current_share_value = df['current_share_value'].sum()
             pnl = current_share_value - previous_share_value 
             performance = ((current_share_value - previous_share_value) / previous_share_value) * 100 if previous_share_value > 0 else 0
 
-            summary_message = ("Summary:\n"
-                               f"Previous Share Value: {previous_share_value:.2f} USD, {previous_share_value * USD_TO_THB:.2f} THB\n"
-                               f"Current Share Value: {current_share_value:.2f} USD, {current_share_value * USD_TO_THB:.2f} THB\n"
+            summary_message = (f"Summary:\n"
+                               f"Previous: {previous_share_value:.2f} USD, {previous_share_value * USD_TO_THB:.2f} THB\n"
+                               f"Current: {current_share_value:.2f} USD, {current_share_value * USD_TO_THB:.2f} THB\n"
                                f"PNL: {pnl:.2f} USD, {pnl * USD_TO_THB:.2f} THB\n"
-                               f"Performance: {performance:.2f}%\n\n")
-            self.send_message(summary_message, platforms=['line'])
+                               f"Performance: {performance:.2f}%\n")
+            messages.append(summary_message)
+
+            if len(messages)> 1:
+                self.send_message(messages, platforms=['line'])
         else:
             print("No data fetched.")
