4bc64a1301f20faedb8fa8191b18d89839aeaef6
JamesBond <introbond.dev@gmail.com>
2024-03-10 11:09:50 +0700

    Refactor DefiProtocol constructor to use 'url' parameter instead of 'api_url'

--- src/protocols/__init__.py
+++ src/protocols/__init__.py
@@ -1,12 +1,12 @@
 import requests
 
 class DefiProtocol:
-    def __init__(self, api_url, **params):
-        self.api_url = api_url
+    def __init__(self, url, **params):
+        self.url = url
         self.params = params
 
     def fetch_data(self):
-        response = requests.get(self.api_url, params=self.params)
+        response = requests.get(self.url, params=self.params)
         if response.status_code == 200:
             return response.json()
         else:
