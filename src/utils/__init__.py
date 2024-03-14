ef64288e83d81957c59b21c330f0603534f984f1
JamesBond <introbond.dev@gmail.com>
2024-03-14 10:56:20 +0700

    Refactor unix_to_humanreadable method to remove weekday from the returned date format

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,10 +4,8 @@ class Utils:
     @staticmethod
     def unix_to_humanreadable(unix_timestamp: int) -> str:
         """
-        Converts a Unix timestamp to a human-readable YYYY-M-D-weekday format.
+        Converts a Unix timestamp to a human-readable YYYY-M-D format.
         """
 
         local_time = time.localtime(int(unix_timestamp))
-        date_part = time.strftime("%Y-%m-%d", local_time)
-        weekday = time.strftime("%A", local_time)  # Full weekday name
-        return f"{date_part}-{weekday}"
+        return time.strftime("%Y-%m-%d", local_time)
