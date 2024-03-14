eeb51f322da89e919eb39da72577ec4ee395a247
JamesBond <introbond.dev@gmail.com>
2024-03-14 19:29:50 +0700

    Add Telegram message sending functionality

--- src/protocols/__init__.py
+++ src/protocols/__init__.py
@@ -71,7 +71,27 @@ class DefiProtocol:
             print("Sending message to Line:", message)
 
     def send_message_to_telegram(self, message):
-        print("Sending message to Telegram:", message)
+        token = os.getenv("TELEGRAM_BOT_TOKEN")
+        chat_id = os.getenv("TELEGRAM_CHAT_ID")
+        if token is None or chat_id is None:
+            print("Error: Telegram bot token or chat ID not found in environment variables.")
+            return
+
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        payload = {
+            "chat_id": chat_id,
+            "text": message
+        }
+
+        try:
+            response = requests.post(url, json=payload)
+            if DEBUG:
+                if response.status_code == 200:
+                    print("Message sent to Telegram successfully.")
+                else:
+                    print(f"Failed to send message to Telegram. Status code: {response.status_code}")
+        except Exception as e:
+            print("An error occurred while sending message to Telegram:", str(e))
 
     def send_message_to_discord(self, message):
         url = os.getenv("DISCORD_WEBHOOK_URL")
